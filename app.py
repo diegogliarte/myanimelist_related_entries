1527cffc3db62d0a6ed74030e8152f68e9567a72
Diego González <diegogliarte@gmail.com>
2020-07-03 11:49:45 +0200

    web related

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+from main import myanimelist_related_entries
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if (request.method == 'POST'):
+        print("Starting request...")
+
+        url = request.form['url']
+        print("Url: ", url)
+
+        types = request.form.getlist('types')
+        if "Both" in types:
+            types = ["Anime", "Manga"]
+        print("Types: ", types)
+
+        excluded_category = request.form.getlist('excluded_category')
+        print("Excl category: ", excluded_category)
+
+        excluded_text = request.form['excluded_text'].split(",")
+        print("Excl text: ", excluded_text)
+
+        included_text = request.form['included_text'].split(",")
+        print("Incl text: ", included_text)
+
+        result, result_hrefs= myanimelist_related_entries(url, excluded_category, excluded_text, included_text, types)
+        print(result)
+
+        result = result.split("\n")
+        result_hrefs = result_hrefs.split("\n")
+
+        zipped_data = zip(result, result_hrefs)
+
+        return render_template('main.html', name='myanimelist', zipped_data=zipped_data)
+
+
+    return render_template('main.html', name='myanimelist')
+
+
+if __name__ == "__main__":
+    app.run()
